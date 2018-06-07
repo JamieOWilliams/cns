300263060c6a1238f5ab9c0b93549e2dfcf492c8
Jamie Williams <mail@jamiewilliams.co.uk>
2018-06-08 00:28:55 +0100

    Cookie consent widget added

--- js/gifcont.js
+++ js/gifcont.js
@@ -1,38 +1,38 @@
-$('#networkingGif').hide();
-$('#backupGif').hide();
-$('#cloudGif').hide();
-
-$('.vid-block').mouseover(function() {
-  var getGif = '#' + this.id + ' .gif'
-  var getPng = '#' + this.id + ' .png'
-  console.log(getPng);
-  console.log(getGif);
-  $(getPng).hide();
-  $(getGif).show();
-  $('#' + this.id + ' .vid-block-text').css('background-color', 'rgba(240, 240, 240, 0.5)')
-  $('#' + this.id + ' .vid-block-text').css('box-shadow', '2px 2px 1px rgba(220, 220, 220, 1)')
-  console.log(this.id + ' .vid-block-text')
-});
-
-$('.vid-block').mouseout(function() {
-  $('#networkingGif').hide();
-  $('#backupGif').hide();
-  $('#cloudGif').hide();
-  $('#networkingPng').show();
-  $('#backupPng').show();
-  $('#cloudPng').show();
-  $('.vid-block-text').css('background-color', 'none')
-  $('#' + this.id + ' .vid-block-text').css('box-shadow', 'none')
-});
-
-function setTop() {
-  var pos = $('#001 .vid-block-text').position()
-  $('#002 .vid-block-text').css('top', pos.top)
-  $('#003 .vid-block-text').css('top', pos.top)
-};
-
-setTop();
-
-$(window).resize(function() {
-  setTop()
-});
+// $('#networkingGif').hide();
+// $('#backupGif').hide();
+// $('#cloudGif').hide();
+//
+// $('.vid-block').mouseover(function() {
+//   var getGif = '#' + this.id + ' .gif'
+//   var getPng = '#' + this.id + ' .png'
+//   console.log(getPng);
+//   console.log(getGif);
+//   $(getPng).hide();
+//   $(getGif).show();
+//   $('#' + this.id + ' .vid-block-text').css('background-color', 'rgba(240, 240, 240, 0.5)')
+//   $('#' + this.id + ' .vid-block-text').css('box-shadow', '2px 2px 1px rgba(220, 220, 220, 1)')
+//   console.log(this.id + ' .vid-block-text')
+// });
+//
+// $('.vid-block').mouseout(function() {
+//   $('#networkingGif').hide();
+//   $('#backupGif').hide();
+//   $('#cloudGif').hide();
+//   $('#networkingPng').show();
+//   $('#backupPng').show();
+//   $('#cloudPng').show();
+//   $('.vid-block-text').css('background-color', 'none')
+//   $('#' + this.id + ' .vid-block-text').css('box-shadow', 'none')
+// });
+//
+// function setTop() {
+//   var pos = $('#001 .vid-block-text').position()
+//   $('#002 .vid-block-text').css('top', pos.top)
+//   $('#003 .vid-block-text').css('top', pos.top)
+// };
+//
+// setTop();
+//
+// $(window).resize(function() {
+//   setTop()
+// });
